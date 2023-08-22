a348afdff53691d72a837a716f1c09fd013c04c5
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-08-21 19:26:25 -0700

    migrate wiki from dawn lit frontend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  private static readonly HOME_SOURCE: string = 'https://raw.githubusercontent.com/LinpgFoundation/linpg/master/README.md'
   title = 'linpg-wiki';
+  readonly WIKI_DOC: Record<string, string> = {
+    "about_en": "About",
+    "about_cn": "关于",
+    "introduction_cn": "简介"
+  }
+  protected currentSource: string = AppComponent.HOME_SOURCE
+
+  protected setCurrentSource(src: string | null): void {
+    this.currentSource = src != null ? `/doc/${src}.md` : AppComponent.HOME_SOURCE
+  }
 }
